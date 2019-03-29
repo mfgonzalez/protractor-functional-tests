e242c7ad9eb68f47967dd0529c021375abe10abc
Marcelo Gonzalez <mfgonzalez@gmail.com>
2019-03-29 10:10:55 -0300

    Inclusão dos critérios de aceitação

--- spec.js
+++ spec.js
@@ -33,6 +33,17 @@ describe('Protractor Demo App', function(){
 		btn.click();
 	};
 
+	/*
+	**	1) Dado que o usuário não digita nada na caixa de texto de pesquisa
+	**	Quando clicar no botão de pesquisa (lupa)
+	**	Então exibir a tela de resultdo de busca com a mensagem "Please enter a search keyword"
+	*/
+
+	/*
+	**	2) Dado que o usuário não informa o e-mail na tela de "sign in"
+	**	Quando clicar no botão "Create an account"
+	**	Então exibir a mensagem "Invalid email address."
+	*/
 	it('Deve informar email no sign in', function(){
 		clicarLinkSignIn();
 		clicarBotaoPorId('SubmitCreate');
@@ -42,30 +53,58 @@ describe('Protractor Demo App', function(){
 		expect($('.alert-danger > ol > li').getText()).toEqual('Invalid email address.');
 	});
 
+	/*
+	**	3) Dado que o usuário não informa o e-mail e Password na tela de "sign in"
+	**	Quando clicar no botão "Sign in"
+	**	Então exibir a mensagem "An email address required."
+	*/
 	it('Deve informar email e password para login', function(){
 		clicarLinkSignIn();
 		clicarBotaoPorId('SubmitLogin');
 		expect($('.alert-danger > ol > li').getText()).toEqual('An email address required.');
 	});
 
+	/*
+	**	4) Dado que o usuário informa o e-mail e não informa Password na tela de "sign in"
+	**	Quando clicar no botão "Sign in"
+	**	Então exibir a mensagem "Password is required."
+	*/
 
-
+	/*
+	**	5) Dado que o usuário não informa o e-mail
+	**	Quando clicar no botão da Newsletter
+	**	Então exibir a mensagem "Newsletter : Invalid email address."
+	*/
 	it('Deve informar email para assinar newsletter', function(){
 		clicarBotaoPorName('submitNewsletter');
 		expect($('.alert-danger').getText()).toEqual('Newsletter : Invalid email address.');
 	});
 
-
+	/*
+	**	6) Dado que o usuário não tem items no carrinho
+	**	Quando clicar no botão "Cart"
+	**	Então deve exibir a mensagem "Your shopping cart is empty."
+	*/
 	it('Carrinho vazio', function(){
 		clicarHref('.shopping_cart');
 		expect($('.alert-warning').getText()).toEqual('Your shopping cart is empty.');
 	});
 
-
+	/*
+	**	7) Dado que o usuário não informa o e-mail 
+	**	Quando clicar no botão "Send" da tela de contato
+	**	Então exibe a mensagem "Invalid email address."
+	*/
 	it('Deve informar email para contato', function(){
 		clicarHref('#contact-link');
 		clicarBotaoPorId('submitMessage');
 		expect($('.alert-danger ol > li').getText()).toEqual('Invalid email address.');
 	});
+
+	/*
+	**	8) Dado que o usuário informa somente o e-mail 
+	**	Quando clicar no botão "Send" da tela de contato
+	**	Então exibe a mensagem "The message cannot be blank."
+	*/
 
 });
